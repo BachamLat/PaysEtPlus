9f03cd3d498c4c398ca1c84a52bcbb67a053753a
amirbab <amirbabio43@gmail.com>
2022-03-25 02:04:29 +0100

    Implement part from field required for all user

--- src/Form/CountriesType.php
+++ src/Form/CountriesType.php
@@ -6,17 +6,44 @@ use App\Entity\Countries;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class CountriesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('code')
-            ->add('dialCode')
-            ->add('longitude')
-            ->add('latitude')
+            ->add('name', TextType::class, [
+                'label'=> 'Name',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('code', TextType::class, [
+                'label'=> 'Code',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('dialCode', TextType::class, [
+                'label'=> 'DialCode',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('longitude', IntegerType::class, [
+                'label' => 'Longitude',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('latitude', IntegerType::class, [
+                'label'=> 'Latitude',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
         ;
     }
 
--- src/Form/TownType.php
+++ src/Form/TownType.php
@@ -6,15 +6,32 @@ use App\Entity\Town;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class TownType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('longitude')
-            ->add('latitude')
+            ->add('name', TextType::class, [
+                'label'=> 'Name',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('longitude', IntegerType::class, [
+                'label' => 'Longitude',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('latitude', IntegerType::class, [
+                'label'=> 'Latitude',
+                'attr'=> [
+                    'class' => 'form-control'
+                ]
+            ])
             ->add('district')
         ;
     }
