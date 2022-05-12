8afc26e9b0c43b2648dc9cc0afa25e8c5470e415
amirbab <amirbabio43@gmail.com>
2022-05-12 17:22:54 +0200

    check each coordinate for one's all module

--- public/css/js/map-init.js
+++ public/css/js/map-init.js
@@ -1,45 +1,34 @@
+function initMap() 
+  {
+    
+    const lati = parseFloat( (document.getElementById("lat").innerHTML).trim() );
+    const long = parseFloat( (document.getElementById("long").innerHTML).trim() );
 
-
-    function initMap() {
-    // const uluru = { lat: 9.3220475, lng: 2.313138 };
-
-    const myLatLng = { lat: 9.3220475, lng:  2.313138};
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatLng,  
-    });
-  
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "Hello World!",
-    });
+    if((lati == 0)&&(long==0)){
+      document.getElementById("map").style.height = "2px";
+      document.getElementById("map").style.textAlign="center";
+      document.getElementById("map").style.fontSize = "18px";
+      document.getElementById("map").style.fontWeight = "bold";
+      document.getElementById("map").style.fontFamily = "Arial-Serif";     
+      document.getElementById("map").innerText="Aucun Emplacement Disponible Sur la Carte Geographique";
+    }
+    else{
+        const myLatLng = { lat: lati, lng: long};
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 4,
+          center: myLatLng,  
+        });
+      
+        new google.maps.Marker({
+          position: myLatLng,
+          map,
+          title: "Hello World!",
+        });
+    }
   }
   window.initMap = initMap;
 
 
-  const click = document.getElementById("clickItem");
-
-  click.addEventListener('onClick', function()
-  {
-    // alert('Click sucess');
-    // event.preventDefault(); 
-    click.innerHTML = "C'est cliqué !"            
-  }, true);
-
-
-  function recupLatLong() 
-  {
-    var latitude = document.getElementById('latitude');
-    var longitude = document.getElementById('longitude');
-
-    if( typeof(latitude) == int && typeof(longitude)==int)
-    {
-      alert(" Variables entiers /n Lat : "+latitude+" Long : "+longitude);
-    }
-    else{
-      lati,longi = parseInt(latitude) , parseInt(longitude);
-      alert(" Variables convertis entiers /n Lat : "+latitude+" Long : "+longitude);
-    }
+  function click(){
+    initMap();
   }
-  window.recupLatLong = recupLatLong;
